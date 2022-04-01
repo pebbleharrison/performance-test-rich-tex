fb4eab33c44ea35fc23fd0c636d44adaa9cae5eb
tony <tony@pebblepad.co.uk>
2022-04-01 11:43:48 +0100

    Updated ckeditor 4 config to not include Pebble only params and...
    added logging for length of time a number of instances takes to load

--- ck-editor-4/scripts.js
+++ ck-editor-4/scripts.js
@@ -1,12 +1,26 @@
 const editors = document.getElementsByClassName('editor');
 CKEDITOR.disableAutoInline = true;
 
-Array.from(editors).forEach((editor) => {
-    CKEDITOR.replace(editor, {
-        ckEditorAttributes: "style,href,title,target,face,size,color,align,start,reversed,type,data-selection-bookmark,data-selection-bookmark-end",
-        removeButtons: "Format,Subscript,Superscript,Table,Image,Maximize,Source,About,Blockquote,Anchor,SpecialChar,HorizontalRule,PasteFromWord,PasteText,Paste,NumberedList,Outdent,Language,Indent,BulletedList,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,paragraphIndent",
-        ckEditorStyles: "font,font-size,font-style,font-variant,font-weight,font-stretch,font-family,color",
-        ckEditorTags: "a strong b i em font u span sub sup small mark cite abbr s strike selection-bookmark",
-        tags: ['a', 'strong', 'b', 'i', 'em', 'font', 'u', 'span', 'sub', 'sup', 'small', 'mark', 'cite', 'abbr', 's', 'strike']
+let callCount = 0;
+
+function editorLoaded() {
+    callCount = callCount + 1;
+    if (callCount === editors.length){
+        performance.measure("measure to last instance loaded", "loading of N instances");
+        console.log(performance.getEntriesByType("measure"));
+    }
+}
+
+
+console.log("editors count", editors.length)
+
+performance.mark("loading of N instances");
+
+Array.from(editors).forEach((editor, i) => {
+    const ckEditor = CKEDITOR.replace(editor, {
+        removeButtons: "Format,Subscript,Superscript,Table,Image,Maximize,Source,About,Blockquote,Anchor,SpecialChar,HorizontalRule,PasteFromWord,PasteText,Paste",
+        removePlugins: "colordialog,floatingspace,link,unlink,linker,colorbutton,formatting,stylescombo,format,font,size,justify"
     });
 });
+
+CKEDITOR.on("instanceReady", editorLoaded)
